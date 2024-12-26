d7a2ca1d0952adc8fc93aee5b1352ed1fc69813f
KeneHermitCoder <kene.nnakwue@notjustevent.com>
2024-12-26 11:59:53 +0000

    chores: updated tables, dropdown, other components...

---
+++ src/components/tables/SearchFilterSortPaginateTable.tsx
@@ -0,0 +1,263 @@
+import React, { useMemo, useState } from "react";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import SearchField from "../searchField";
+import { visuallyHidden } from "@mui/utils";
+import DropDownSelect from "../DropDownSelect";
+import { TableRow, Stack } from "@mui/material";
+import TableBody from "@mui/material/TableBody";
+import TableHead from "@mui/material/TableHead";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import TablePagination from "@mui/material/TablePagination";
+
+interface Data {
+  [key: string]: any;
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+interface EnhancedTableProps {
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+  headCells: HeadCell[];
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort, headCells } = props;
+  const createSortHandler =
+    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={"normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export default function SearchFilterSortPaginateTable({
+  rows = [],
+  headCells = [],
+  // filterParams = [],
+  title = "Table Title",
+}: {
+  rows?: Data[];
+  title?: string;
+  filterParams?: any[];
+  searchParams: any[];
+  headCells?: HeadCell[];
+}) {
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState<Order>("asc");
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [orderBy, setOrderBy] = useState<keyof Data>("calories");
+  const [filteredRows, setFilteredRows] = useState<Data[]>(rows);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => {
+    console.log({ event });
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    console.log({ event });
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredRows.length) : 0;
+
+  const visibleRows = useMemo(
+    () =>
+      [...filteredRows]
+        .sort(getComparator(order, orderBy))
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [order, orderBy, page, rowsPerPage, filteredRows]
+  );
+
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value.toLowerCase();
+    console.log(searchTerm);
+
+    // Filter out rows based on search params
+    const filtered = rows.filter((row) => {
+      return Object.values(row).some((value) =>
+        value.toString().toLowerCase().includes(searchTerm)
+      );
+    });
+
+    console.log({ filtered });
+    // Set the filtered values to filteredRows
+    setFilteredRows(filtered);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }} className="flex flex-col gap-3">
+      <div className="flex flex-col lg:flex-row md:justify-between gap-2">
+        <div className="text-xl text-gray-700">{title}</div>
+        <Stack
+          direction="row"
+          spacing={1}
+          className="justify-between items-center"
+        >
+          {/* Search Field */}
+          <SearchField onChange={onSearch} />
+          <div className="flex justify-end gap-1">
+            <DropDownSelect
+              options={["Today", "Yesterday"]}
+              onSelected={(option: any) => console.log(option)}
+            />
+            <DropDownSelect
+              options={["Pending", "Completed"]}
+              onSelected={(option: any) => console.log(option)}
+            />
+          </div>
+        </Stack>
+      </div>
+      <Stack className="bg-white rounded-[8px] border" spacing={1}>
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy as string}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+              headCells={headCells}
+            />
+            <TableBody>
+              {visibleRows.map((row) => {
+                // const isItemSelected = selected.includes(row.id);
+                // const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    // onClick={(event) => handleClick(row.id, event)}
+                    role="checkbox"
+                    // aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.id}
+                    // selected={isItemSelected}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    {headCells.map((cell, index) => {
+                      return (
+                        <TableCell
+                          key={cell.id}
+                          align={cell.numeric ? "right" : "left"}
+                        >
+                          {index === 0 && row?.metadata?.itemPhoto ? (
+                            <div className="flex items-center gap-2">
+                              <span className="flex w-8 h-8 bg-gray-400 rounded-full overflow-hidden">
+                                <img
+                                  alt=""
+                                  className="w-8 h-8 rounded-full"
+                                  src={row?.metadata?.itemPhoto || ""}
+                                />
+                              </span>
+                              {row[cell.id]}
+                            </div>
+                          ) : (
+                            row[cell.id]
+                          )}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Stack>
+    </Box>
+  );
+}
--- src/routes/loans/Loans.tsx
+++ src/routes/loans/Loans.tsx
@@ -1,24 +1,22 @@
-import { Divider, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import UsersKPIDisplay from "../../components/usersKPI";
-import { DropDownSelect, MainTable, Reveal } from "../../components";
 import {
-  AttachMoney,
-  FlagCircleRounded,
-  HandshakeRounded,
+  Reveal,
+  SearchFilterSortPaginateTable,
+} from "../../components";
+import {
   PersonAdd,
+  AttachMoney,
+  HandshakeRounded,
+  FlagCircleRounded,
 } from "@mui/icons-material";
-import SearchField from "../../components/searchField";
 
 export default function Loans() {
   return (
     <>
       <Reveal>
         <Stack direction="column" spacing={3} paddingX={1} paddingY={1}>
-          <Stack
-            direction="row"
-            sx={{ backgroundColor: "#fff", padding: "12px" }}
-            divider={<Divider orientation="vertical" flexItem />}
-          >
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:divide-x-2 divide-y-2 lg:divide-y-0">
             <UsersKPIDisplay
               subtitle="Total Users"
               kpiIcon={<AttachMoney sx={{ color: "success.main" }} />}
@@ -42,124 +40,97 @@ export default function Loans() {
               kpiIcon={<PersonAdd sx={{ color: "primary.main" }} />}
               total="1,200"
             />
-          </Stack>
+          </div>
 
           <Stack
             spacing={1}
             justifyContent="space-between"
             className="bg-white p-4 rounded-[12px]"
           >
-            <div className='flex flex-col lg:flex-row md:justify-between gap-2'>
-              <div className="text-xl">Bills Overview</div>
-              <Stack direction="row" spacing={1} className=''>
-                {/* Search Field */}
-                <SearchField />
-                <div className="flex w-full">
-                  <DropDownSelect
-                    options={[
-                      'Today',
-                      'Yesterday',
-                    ]}
-                    onSelected={(option: any) => console.log(option)}
-                  />
-                  <DropDownSelect
-                    options={[
-                      'Today',
-                      'Yesterday',
-                    ]}
-                    onSelected={(option: any) => console.log(option)}
-                  />
-                  <DropDownSelect
-                    options={[
-                      'Today',
-                      'Yesterday',
-                    ]}
-                    onSelected={(option: any) => console.log(option)}
-                  />
-                </div>
-              </Stack>
-            </div>
-            <Stack className="border border-gray-200 rounded-[12px] divide-y">
-              <MainTable
-                key="airtime-3bdd92d-1e3e-4d7d-8a1e-6d7c0a0a0a0a"
-                title="Loan Overview"
-                headCells={[
-                  {
-                    id: "customerName",
-                    numeric: false,
-                    label: "Name",
+            <SearchFilterSortPaginateTable
+              key="airtime-3bdd92d-1e3e-4d7d-8a1e-6d7c0a0a0a0a"
+              title="Loan Overview"
+              searchParams={[
+                'customerName',
+                'loanId',
+                'status'
+              ]}
+              headCells={[
+                {
+                  id: "customerName",
+                  numeric: false,
+                  label: "Name",
+                },
+                {
+                  id: "loanId",
+                  numeric: true,
+                  label: "Loan Id",
+                },
+                {
+                  id: "amount",
+                  numeric: true,
+                  label: "Amount",
+                },
+                {
+                  id: "interest",
+                  numeric: true,
+                  label: "Interest",
+                },
+                {
+                  id: "dueDate",
+                  numeric: false,
+                  label: "Due Date",
+                },
+                {
+                  id: "status",
+                  numeric: false,
+                  label: "Status",
+                },
+                {
+                  id: "actions",
+                  numeric: false,
+                  label: "Actions",
+                },
+              ]}
+              rows={[
+                {
+                  customerName: "Kene Nnakwue",
+                  loanId: "LN12345",
+                  amount: "₦2,000",
+                  interest: "₦200",
+                  dueDate: "10/01/2025",
+                  status: "active",
+                  metadata: {
+                    itemPhoto:
+                      "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
                   },
-                  {
-                    id: "loanId",
-                    numeric: true,
-                    label: "Loan Id",
+                },
+                {
+                  customerName: "Chris Obi",
+                  loanId: "LN12346",
+                  amount: "₦10,000",
+                  interest: "₦100",
+                  dueDate: "10/12/2025",
+                  status: "overdue",
+                  metadata: {
+                    itemPhoto:
+                      "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
                   },
-                  {
-                    id: "amount",
-                    numeric: true,
-                    label: "Amount",
-                  },
-                  {
-                    id: "interest",
-                    numeric: true,
-                    label: "Interest",
-                  },
-                  {
-                    id: "dueDate",
-                    numeric: false,
-                    label: "Due Date",
-                  },
-                  {
-                    id: "status",
-                    numeric: false,
-                    label: "Status",
-                  },
-                  {
-                    id: "actions",
-                    numeric: false,
-                    label: "Actions",
-                  },
-                ]}
-                rows={[
-                  {
-                    customerName: "Kene Nnakwue",
-                    loanId: "LN12345",
-                    amount: "₦2,000",
-                    interest: "₦200",
-                    dueDate: "10/01/2025",
-                    status: "active",
-                    metadata: {
-                      itemPhoto:
-                        "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
-                    },
-                  },
-                  {
-                    customerName: "Chris Obi",
-                    loanId: "LN12346",
-                    amount: "₦10,000",
-                    interest: "₦100",
-                    dueDate: "10/12/2025",
-                    status: "overdue",
-                    metadata: {
-                      itemPhoto:
-                        "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
-                    },
-                  },
-                ]}
-              />
-              {/* <MainTable
+                },
+              ]}
+            />
+            {/* <SearchFilterSortPaginateTable
                     key="gift-card-3bdd92d-1e3e-4d7d-8a1e-6d7c0a0a0a0a"
                     onClick={(e: React.MouseEvent) => {
                       console.log(`clicked ${e.target}`);
                     }}
                   />
-                  <MainTable
+                  <SearchFilterSortPaginateTable
                     key="internet-3bdd92d-1e3e-4d7d-8a1e-6d7c0a0a0a0a"
                     onClick={(e: React.MouseEvent) => {
                       console.log(`clicked ${e.target}`);
                     }}
                   /> */}
-            </Stack>
           </Stack>
         </Stack>
       </Reveal>
