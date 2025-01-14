677b4453afac553cd65662205c86c1301f70a753
KeneHermitCoder <kene.nnakwue@notjustevent.com>
2025-01-14 05:52:24 +0000

    updates...

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 export * from './format';
 export * from './windowDimensions';
 export * from './handleRequestStates';
+// export { default as uploadFile, } from './uploadFile';
 export { default as handleError, } from './handleError';
 export { default as supabaseClient, } from './supabaseClient';
+export { default as toggleUnderline, } from './toggleUnderline';
 export { default as tableFilterAction, } from './tableFilterAction';
